a502511d81d61b1ca2bd3ff58c47135c66bc6672
testcrmisoskele <124690577+testcrmisoskele@users.noreply.github.com>
2026-07-10 16:09:28 +0200

    Update streamlit_app.py
    smoothiefroot_response.json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ from snowflake.snowpark.functions import col
 
 # New section to display smoothiefroot nutrition information
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
